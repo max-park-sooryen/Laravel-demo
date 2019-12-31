7f03c2cbb0ad117e0ce51e2bad4b237b2bb603fa
max-park-sooryen <maxpark@Maxs-MBP.home>
2019-12-31 18:57:46 -0500

    typo google secret

--- config/services.php
+++ config/services.php
@@ -35,7 +35,7 @@ return [
      */
     'google' => [
         'client_id'     => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'client_secret' => env('GOOGLE_SECRET'),
         'redirect'      => env('GOOGLE_REDIRECT'),
     ],
 
